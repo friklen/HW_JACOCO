1e07e15aee2ca0dd1bebae7930a61766cb2b2c58
friklen <friklen@gmail.com>
2024-04-25 22:16:15 +0400

    added new test

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -1,8 +1,7 @@
-package ru.netology.statistic;
-
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Assertions;
+import ru.netology.statistic.StatisticsService;
 
 public class StatisticsServiceTest {
 
@@ -12,6 +11,17 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxFromMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 100, 3, 8, 6, 11, 11, 12};
+        long expected = 100;
 
         long actual = service.findMax(incomesInBillions);
 
